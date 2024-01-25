48e26fd5d9a20887fa3b8f04fad78f461f2963bf
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-25 16:57:17 +0300

    creates Employee class

---
+++ src/com/workintech/company/Employee.java
@@ -0,0 +1,49 @@
+package com.workintech.company;
+
+import java.util.Objects;
+
+public class Employee {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
